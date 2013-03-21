9d3b37557d57837dd19e767d30222970b35afc39
Florent Henry <florent.henry@gmail.com>
2013-03-21 13:47:39 +0100

    Ajout de la lecture d'une ligne

--- src/domain/Graph.java
+++ src/domain/Graph.java
@@ -1,29 +1,28 @@
 package domain;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Graph implements IGraph {
 
-    private HashSet<Node> nodes;
+    private HashMap<String, Node> nodes;
 
     public Graph() {
-        this.nodes = new HashSet();
+        this.nodes = new HashMap<>();
     }
 
     @Override
     public void addNode(Node node) {
-        nodes.add(node);
+        nodes.put(node.getId(), node);
     }
 
+    @Override
     public void includeFile(String filePath) {
         FileReader fr = null;
         BufferedReader br = null;
@@ -52,6 +51,25 @@ public class Graph implements IGraph {
         }
     }
 
+    @Override
     public void addLine(String line) {
+        //TODO: Sprint 2 check line validity and continue if false (ex: regex)
+        line = line.toLowerCase();
+        int fromNbr = line.indexOf("--"), toNbr = line.indexOf("-->") + 3;
+        String fromStr = line.substring(0, fromNbr), toStr = line.substring(toNbr);
+        line = line.substring(fromNbr, toNbr);
+        String linkType = line.substring(0, line.indexOf("["));
+
+        Node to = nodes.get(toStr);
+        if (to == null) {
+            to = new Node(toStr);
+        }
+        Node from = nodes.get(fromStr);
+        if (from == null) {
+            from = new Node(fromStr);
+        }
+        Link link = new Link(linkType, from, to);
+        to.addLink(link);
+        from.addLink(link);
     }
 }
