7d44c5681e8c6a483da9267721490be8fea448d0
Julien Neuhart <neuhart.julien@gmail.com>
2013-03-21 12:21:38 +0100

    Mise en place des tests unitaires

---
+++ test/domain/LinkTest.java
@@ -0,0 +1,62 @@
+package domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Julien Neuhart
+ */
+public class LinkTest {
+    
+    public LinkTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getType method, of class Link.
+     */
+    @Test
+    public void testGetType() {
+        System.out.println("getType");
+        Link instance = null;
+        String expResult = "";
+        String result = instance.getType();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Link.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Link instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
---
+++ test/domain/NodeTest.java
@@ -0,0 +1,75 @@
+package domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Julien Neuhart
+ */
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Node.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Node instance = null;
+        String expResult = "";
+        String result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addLink method, of class Node.
+     */
+    @Test
+    public void testAddLink() {
+        System.out.println("addLink");
+        Link link = null;
+        Node instance = null;
+        instance.addLink(link);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Node.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Node instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
