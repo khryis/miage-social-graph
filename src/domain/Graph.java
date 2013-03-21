5e72f3d546d37baba1d47613863a6d9625cee913
Florent Henry <florent.henry@gmail.com>
2013-03-21 13:16:00 +0100

    Lecture du fichier

--- src/domain/Graph.java
+++ src/domain/Graph.java
@@ -1,6 +1,15 @@
 package domain;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Graph implements IGraph {
 
@@ -12,5 +21,36 @@ public class Graph implements IGraph {
 
     public void addNode(Node node) {
         nodes.add(node);
+    }
+
+    public void includeFile(String filePath) {
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(new File(filePath));
+            br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                addLine(line);
+            }
+
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Graph.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Graph.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                br.close();
+                fr.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Graph.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void addLine(String line) {
     }
 }
