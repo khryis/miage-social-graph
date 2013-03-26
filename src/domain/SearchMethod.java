5543997c561bea132670b48b00bfc5211f20be75
Florent Henry <florent.henry@gmail.com>
2013-03-26 10:33:12 +0100

    Amélioration de l'Enum SearchMethod (ajout des descriptions)

--- src/domain/SearchMethod.java
+++ src/domain/SearchMethod.java
@@ -2,5 +2,16 @@ package domain;
 
 public enum SearchMethod {
 
-    DFS, BFS;
+    DFS("Depth-first search (default)"),
+    BFS("Breadth-first search");
+    String description;
+
+    SearchMethod(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
