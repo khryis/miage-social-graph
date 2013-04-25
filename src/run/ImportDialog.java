f92bd30851cd3d1f8604162b5ac1950b6865479e
Loïc Mounier <loicmounier@live.fr>
2013-04-26 00:59:38 +0200

    develop-run modif en tout genre

---
+++ src/run/ImportDialog.java
@@ -0,0 +1,89 @@
+package run;
+
+import domain.Graph;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class ImportDialog extends JDialog {
+
+    private ImportDialogInfo zInfo = new ImportDialogInfo();
+    private Graph graph;
+    private JRadioButton strict, noStrict, ecrase, update;
+
+    public ImportDialog(JFrame parent, String title, boolean modal, Graph g) {
+        super(parent, title, modal);
+        graph = g;
+        this.setSize(550, 80);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        this.initComponent();
+    }
+
+    public ImportDialogInfo showZDialog() {
+        this.setVisible(true);
+        return this.zInfo;
+    }
+
+    private void initComponent() {
+        //Paramètre d'import
+        JPanel panParam = new JPanel();
+        panParam.setBackground(Color.white);
+        panParam.setPreferredSize(new Dimension(440, 60));
+        panParam.setBorder(BorderFactory.createTitledBorder("Paramètres d'importation"));
+        ButtonGroup bg1 = new ButtonGroup();
+        strict = new JRadioButton("Strict", false);
+        bg1.add(strict);
+        noStrict = new JRadioButton("Pas strict", false);
+        bg1.add(noStrict);
+        panParam.add(strict);
+        panParam.add(noStrict);
+        ButtonGroup bg2 = new ButtonGroup();
+        ecrase = new JRadioButton("Ecrase", false);
+        bg2.add(ecrase);
+        update = new JRadioButton("Update", false);
+        bg2.add(update);
+        panParam.add(ecrase);
+        panParam.add(update);
+
+        // Création du panel
+        JPanel content = new JPanel();
+        content.setBackground(Color.white);
+        content.add(panParam);
+
+        JPanel control = new JPanel();
+        control.setBackground(Color.white);
+        JButton okBouton = new JButton("OK");
+        okBouton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                zInfo = new ImportDialogInfo(strict.isSelected(), noStrict.isSelected(), ecrase.isSelected(), update.isSelected());
+                setVisible(false);
+            }
+        });
+
+        JButton cancelBouton = new JButton("Annuler");
+        cancelBouton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                zInfo = null;
+                setVisible(false);
+            }
+        });
+        control.add(okBouton);
+        control.add(cancelBouton);
+
+        this.getContentPane().add(content, BorderLayout.CENTER);
+        this.getContentPane().add(control, BorderLayout.SOUTH);
+    }
+}
--- src/run/Interface.java
+++ src/run/Interface.java
@@ -33,6 +33,8 @@ import search.IGraphParser.Unicity;
 import search.SearchException;
 import search.SearchMethod;
 import search.SearchResult;
+import run.ImportDialog;
+import run.ImportDialogInfo;
 
 public class Interface extends JPanel implements ActionListener {
 
@@ -77,6 +79,10 @@ public class Interface extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == importButton) {
+
+            ImportDialog zd1 = new ImportDialog(null, "Rechercher", true, g);
+            //ImportDialogInfo zInfo1 = zd1.showZDialog();
+
             int returnVal = fc.showOpenDialog(Interface.this);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 file = fc.getSelectedFile();
@@ -101,132 +107,134 @@ public class Interface extends JPanel implements ActionListener {
         } else if (e.getSource() == searchButton) {
             SearchDialog zd = new SearchDialog(null, "Rechercher", true, g);
             SearchDialogInfo zInfo = zd.showZDialog();
-            log.append("Search: \n" + zInfo.toString() + newline);
-            String startNode = zInfo.getStartNode();
-            String searchMethod = zInfo.getSearchMethod();
-            int searchLevel = zInfo.getSearchLevel();
-            String unicityStr = zInfo.getUnicity();
-            Unicity unicity = null;
-            if (unicityStr.equals("GLOBAL NODE")) {
-                unicity = Unicity.GLOBALNODE;
-            } else {
-                unicity = Unicity.GLOBALRELATION;
-            }
-            String[] links = zInfo.getLinks();
-            List<LinkFilter> filters = new ArrayList<>();
-            for (String link : links) {
-                if (!link.isEmpty()) {
-                    LinkFilter linkFilter;
-                    String linkType;
-                    Map<String, List<String>> attributes = null;
-                    int direction = 0;
-                    // Has attributes on links
-                    if (link.contains("[")) {
-                        linkType = link.substring(0, link.indexOf("["));
-                        String attributesLine = link.substring(link.indexOf("[") + 1, link.lastIndexOf("]"));
-                        // Gets attributes values
-                        String regex = "((\\w+=(\\[((\\w+)|\\|)+\\]|\\w+)))";
-                        Pattern pattern = Pattern.compile(regex);
-                        Matcher matcher = pattern.matcher(attributesLine);
-                        String attribute, key;
-                        List<String> values = new ArrayList<>();
-                        attributes = new HashMap<>();
-                        while (matcher.find()) {
-                            // attribute : "since=1999" or "share=[book|movie]"
-                            attribute = attributesLine.substring(matcher.start(), matcher.end());
-                            // key = "since"
-                            key = attribute.substring(0, attribute.indexOf("="));
-                            if (attribute.contains("[")) {
-                                //"[book|movie]"
-                                //values = Arrays.asList(attribute.substring(attribute.indexOf("[") + 1, attribute.length() - 1).split("\\|"))
-                                String tmp = attribute.substring(attribute.indexOf("[") + 1, attribute.length() - 1);
-                                String[] tabTmp = tmp.split("|");
-                                for (String att : tabTmp) {
-                                    values.add(att);
-                                }
-                            } else {
-                                //"1999"
-                                values.add(attribute.substring(attribute.indexOf("=") + 1));
-                            }
-                            attributes.put(key, values);
-                        }
-                        if (link.contains(">") && link.contains(">")) {
-                            direction = 3;
-                        } else if (link.contains(">")) {
-                            direction = 2;
-                        } else if (link.contains("<")) {
-                            direction = 1;
-
-                        }
-                    } else {
-                        if (link.contains(">") && link.contains(">")) {
-                            linkType = link.substring(0, link.indexOf(">"));
-                            direction = 3;
-                        } else if (link.contains(">")) {
-                            linkType = link.substring(0, link.indexOf(">"));
-                            direction = 2;
-                        } else if (link.contains("<")) {
-                            linkType = link.substring(0, link.indexOf("<"));
-                            direction = 1;
-                        } else {
-                            linkType = link;
-                        }
-                    }
-                    switch (direction) {
-                        case 1:
-                            linkFilter = new LinkFilter(linkType, Direction.IN);
-                            break;
-                        case 2:
-                            linkFilter = new LinkFilter(linkType, Direction.OUT);
-                            break;
-                        case 3:
-                            linkFilter = new LinkFilter(linkType, Direction.BOTH);
-                            break;
-                        default:
-                            linkFilter = new LinkFilter(linkType);
-                    }
-
-                    if (attributes != null) {
-                        for (String key : attributes.keySet()) {
-                            linkFilter.addAttribute(key, attributes.get(key));
-                        }
-                    }
-
-                    filters.add(linkFilter);
+            if (zInfo != null) {
+                log.append("Search: \n" + zInfo.toString() + newline);
+                String startNode = zInfo.getStartNode();
+                String searchMethod = zInfo.getSearchMethod();
+                int searchLevel = zInfo.getSearchLevel();
+                String unicityStr = zInfo.getUnicity();
+                Unicity unicity = null;
+                if (unicityStr.equals("GLOBAL NODE")) {
+                    unicity = Unicity.GLOBALNODE;
+                } else {
+                    unicity = Unicity.GLOBALRELATION;
                 }
+                String[] links = zInfo.getLinks();
+                List<LinkFilter> filters = new ArrayList<>();
+                for (String link : links) {
+                    if (!link.isEmpty()) {
+                        LinkFilter linkFilter;
+                        String linkType;
+                        Map<String, List<String>> attributes = null;
+                        int direction = 0;
+                        // Has attributes on links
+                        if (link.contains("[")) {
+                            linkType = link.substring(0, link.indexOf("["));
+                            String attributesLine = link.substring(link.indexOf("[") + 1, link.lastIndexOf("]"));
+                            // Gets attributes values
+                            String regex = "((\\w+=(\\[((\\w+)|\\|)+\\]|\\w+)))";
+                            Pattern pattern = Pattern.compile(regex);
+                            Matcher matcher = pattern.matcher(attributesLine);
+                            String attribute, key;
+                            List<String> values = new ArrayList<>();
+                            attributes = new HashMap<>();
+                            while (matcher.find()) {
+                                // attribute : "since=1999" or "share=[book|movie]"
+                                attribute = attributesLine.substring(matcher.start(), matcher.end());
+                                // key = "since"
+                                key = attribute.substring(0, attribute.indexOf("="));
+                                if (attribute.contains("[")) {
+                                    //"[book|movie]"
+                                    //values = Arrays.asList(attribute.substring(attribute.indexOf("[") + 1, attribute.length() - 1).split("\\|"))
+                                    String tmp = attribute.substring(attribute.indexOf("[") + 1, attribute.length() - 1);
+                                    String[] tabTmp = tmp.split("|");
+                                    for (String att : tabTmp) {
+                                        values.add(att);
+                                    }
+                                } else {
+                                    //"1999"
+                                    values.add(attribute.substring(attribute.indexOf("=") + 1));
+                                }
+                                attributes.put(key, values);
+                            }
+                            if (link.contains(">") && link.contains(">")) {
+                                direction = 3;
+                            } else if (link.contains(">")) {
+                                direction = 2;
+                            } else if (link.contains("<")) {
+                                direction = 1;
 
-            }
-            GraphParser parser = new GraphParser(g);
-            switch (searchMethod) {
-                case "DFS":
-                    try {
-                        SearchResult result;
-                        if (searchLevel > 0) {
-                            result = parser.search(startNode, filters, SearchMethod.DFS, searchLevel, unicity);
+                            }
                         } else {
-                            result = parser.search(startNode, filters, SearchMethod.DFS, unicity);
+                            if (link.contains(">") && link.contains("<")) {
+                                linkType = link.substring(0, link.indexOf(">"));
+                                direction = 3;
+                            } else if (link.contains(">")) {
+                                linkType = link.substring(0, link.indexOf(">"));
+                                direction = 2;
+                            } else if (link.contains("<")) {
+                                linkType = link.substring(0, link.indexOf("<"));
+                                direction = 1;
+                            } else {
+                                linkType = link;
+                            }
                         }
-                        log.append(result.toString() + newline);
-                    } catch (SearchException ex) {
-                        Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    break;
-                case "BFS":
-                    try {
-                        SearchResult result;
-                        if (searchLevel > 0) {
-                            result = parser.search(startNode, filters, SearchMethod.DFS, searchLevel, unicity);
-                        } else {
-                            result = parser.search(startNode, filters, SearchMethod.DFS, unicity);
+                        switch (direction) {
+                            case 1:
+                                linkFilter = new LinkFilter(linkType, Direction.IN);
+                                break;
+                            case 2:
+                                linkFilter = new LinkFilter(linkType, Direction.OUT);
+                                break;
+                            case 3:
+                                linkFilter = new LinkFilter(linkType, Direction.BOTH);
+                                break;
+                            default:
+                                linkFilter = new LinkFilter(linkType);
                         }
-                        log.append(result.toString() + newline);
-                    } catch (SearchException ex) {
-                        Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+
+                        if (attributes != null) {
+                            for (String key : attributes.keySet()) {
+                                linkFilter.addAttribute(key, attributes.get(key));
+                            }
+                        }
+
+                        filters.add(linkFilter);
                     }
-                    break;
+
+                }
+                GraphParser parser = new GraphParser(g);
+                switch (searchMethod) {
+                    case "DFS":
+                        try {
+                            SearchResult result;
+                            if (searchLevel > 0) {
+                                result = parser.search(startNode, filters, SearchMethod.DFS, searchLevel, unicity);
+                            } else {
+                                result = parser.search(startNode, filters, SearchMethod.DFS, unicity);
+                            }
+                            log.append(result.toString() + newline);
+                        } catch (SearchException ex) {
+                            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        break;
+                    case "BFS":
+                        try {
+                            SearchResult result;
+                            if (searchLevel > 0) {
+                                result = parser.search(startNode, filters, SearchMethod.DFS, searchLevel, unicity);
+                            } else {
+                                result = parser.search(startNode, filters, SearchMethod.DFS, unicity);
+                            }
+                            log.append(result.toString() + newline);
+                        } catch (SearchException ex) {
+                            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        break;
+                }
+            } else if (e.getSource()
+                    == exportButton) {
             }
-        } else if (e.getSource()
-                == exportButton) {
         }
     }
 }
